0c4392e29c517d7216b04ec67d594702bc833caf
marko <marko.laban@l33tsystems.com>
2020-09-04 09:09:32 +1200

    Adding pypi package required files

--- pycli/__init__.py
+++ pycli/__init__.py
@@ -1,2 +1,5 @@
 from .command import *
 from .shell import *
+
+__version__ = "1.0.0"
+
---
+++ setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="pycli",
+    version="1.0.0",
+    description="Build your CLI with ease",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/markolaban/pycli",
+    author="Marko Laban",
+    author_email="makro.laban@l33tsystems.com",
+    license="BSD",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["pycli"],
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "pycli=pycli.__main__:main",
+        ]
+    },
+)

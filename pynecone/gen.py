383f30c54aae028d7302d3580456ec10182842eb
marko <marko.laban@l33tsystems.com>
2020-09-18 07:00:52 +1200

    Implemented gen command via jinja2

--- pynecone/gen.py
+++ pynecone/gen.py
@@ -1,4 +1,8 @@
 from .cmd import Cmd
+
+import os
+from pathlib import Path
+
 from jinja2 import Environment, PackageLoader, select_autoescape
 env = Environment(
     loader=PackageLoader('pynecone', 'templates'),
@@ -11,12 +15,36 @@ class Gen(Cmd):
         super().__init__("gen")
 
     def add_arguments(self, parser):
-        parser.add_argument('op', choices=['cmd'],
+        parser.add_argument('op', choices=['cmd', 'shell'],
                             help="authenticate", default='cmd', const='cmd', nargs='?')
+        parser.add_argument('names', help="command names", nargs='+')
+        parser.add_argument('--output_folder', help="use the specified output folder", default=os.path.join(os.getcwd(), 'commands'))
 
     def run(self, args):
-        template = env.get_template('cmd.jinja')
-        print(template.render(class_name='TestName', go='testname'))
+        if args.op == 'shell':
+            shell_name = args.names[0]
+            cmd_names = args.names[1:]
+            shell_template = env.get_template('shell.jinja')
+            cmd_template = env.get_template('cmd.jinja')
+
+            Path(args.output_folder).mkdir(parents=True, exist_ok=True)
+
+            with open(os.path.join(args.output_folder, shell_name.lower() + '.py'), "w") as fh:
+                fh.write(shell_template.render(class_name=shell_name.title(), name=shell_name.lower(), names=[cmd.lower() for cmd in cmd_names]))
+
+            for cmd_name in cmd_names:
+                with open(os.path.join(args.output_folder, cmd_name.lower() + '.py'), "w") as fh:
+                    fh.write(cmd_template.render(class_name=cmd_name.title(), name=cmd_name.lower()))
+        else:
+            Path(args.output_folder).mkdir(parents=True, exist_ok=True)
+
+            cmd_template = env.get_template('cmd.jinja')
+
+            cmd_names = args.names
+
+            for cmd_name in cmd_names:
+                with open(os.path.join(args.output_folder, cmd_name.lower() + '.py'), "w") as fh:
+                    fh.write(cmd_template.render(class_name=cmd_name.title(), name=cmd_name.lower()))
 
     def get_help(self):
         return 'generate a new command'

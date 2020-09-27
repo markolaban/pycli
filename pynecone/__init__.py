7caeca1b2449f52917d5fe1bedc72aa52d28600f
marko <marko.laban@l33tsystems.com>
2020-09-28 08:12:55 +1300

    WIP modules - fixing bugs

--- pynecone/__init__.py
+++ pynecone/__init__.py
@@ -1,13 +1,14 @@
 from .cmd import *
 from .shell import *
-from .folder import *
-from .mount import *
 from .auth import *
 from .api import *
 from .taskcmd import *
 from .rest import *
 from .module import *
+from .config import *
+from .proto import *
 from .test import *
 
-__version__ = "0.0.43"
+
+__version__ = "0.0.44"
 
